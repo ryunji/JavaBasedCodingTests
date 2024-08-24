3a0debe60cd9b14a8e024c28cc0fc6b4baaa8cee
ryunji1990@gmail.com <ryunji1990@gmail.com>
2024-08-24 23:35:04 +0900

    주석 수정

--- Stack/ValidParentheses.java
+++ Stack/ValidParentheses.java
@@ -11,14 +11,14 @@ public class ValidParentheses {
         Stack<Character> stack = new Stack<>();
         char[] pt = params.toCharArray();
         for(char c : pt){
-
+            //System.out.println("c : " + c);
             if(c == '(')
                 stack.push(c);
             else
-                if(stack.isEmpty() || stack.pop() == c)         // c가 '('가 아니면 else 문에서 stack.pop()을 해주어야 한다.
-                    return false;                               // 이 부분을 타는건 현재 c의 값이 ')'인데, stack.isEmpty()이면 ')'과 짝지을 '('이게 없는 것이고
-        }                                                       // stack.pop() == c 이 경우는 있을 수가 없음. 그렇기 때문에 return false를 타지 않을 것임.
-                                                                // stack.pop() == c는 stack이 비어있지 않음을 뜻함.
+                if(stack.isEmpty() || stack.pop() == c)         // 1. c가 '('가 아니면 else 문에서 stack이 비어있는지 체크하거나 stack.pop()을 해주어야 한다.
+                    return false;                               // 2. else 부분을 타는 경우는 현재 c의 값이 ')'인데, stack.isEmpty()이면 ')'과 짝지을 '('이게 없는 것.
+        }                                                       // stack.pop() == c, 이 경우는 있을 수 없지만 pop()을 하기 위한 목적으로
+                                                                // else문에는 비교문이 들어가야 하기 때문에 == c를 통해 이를 충족한 것이다. 
         boolean isEmpty = stack.isEmpty();
         return isEmpty;                                         // true or false 반환.
     }
